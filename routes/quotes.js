afa0779bd33068af6193ed80c35bde4b452576d1
Ian Pollack <ianjpollack@gmail.com>
2024-11-10 23:50:29 -0800

    setup get request for list of quotes from quotes.db with pagination

---
+++ routes/quotes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const quotes = require('../services/quotes');
+
+router.get('/', function(req, res, next) {
+  try {
+    res.json(quotes.getMultiple(req.query.page));
+  } catch(err) {
+    console.error(`Error while getting quotes `, err.message);
+    next(err);
+  };
+});
+
+module.exports = router;
